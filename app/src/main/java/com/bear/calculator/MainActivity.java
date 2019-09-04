64609f888b9ab327a1a69d260ff65c3bc086a92d
VIP-bear <1907464379@qq.com>
2019-09-04 14:01:19 +0800

    Implemented basic functions

--- app/src/main/java/com/bear/calculator/MainActivity.java
+++ app/src/main/java/com/bear/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.bear.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,13 +34,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button num_8;           // 8
     private Button num_9;           // 9
 
+    private String ope = "";        // 运算符
+    private String inputNum_1 = "";      // 第一个数
+    private String inputNum_2 = "";      // 第二个数
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initLayout();       // 初始化变量
-
+        setClickEvent();    // 设置监听器
 
     }
 
@@ -48,16 +55,116 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
             case R.id.clear:
                 text.setText("0");
+                ope = "";
+                inputNum_1 = "";
+                inputNum_2 = "";
                 break;
             case R.id.neg:
-                text.setText(negate(text.getText().toString()));
+                text.setText(negate());
+                break;
+            case R.id.per:
+                text.setText(div_100());
+                break;
+            case R.id.num_0:
+            case R.id.num_1:
+            case R.id.num_2:
+            case R.id.num_3:
+            case R.id.num_4:
+            case R.id.num_5:
+            case R.id.num_6:
+            case R.id.num_7:
+            case R.id.num_8:
+            case R.id.num_9:
+            case R.id.point:
+                showNum((Button)v);
+                break;
+            case R.id.add:
+            case R.id.sub:
+            case R.id.mul:
+            case R.id.div:
+                ope = ((Button) v).getText().toString();
+                inputNum_1 = text.getText().toString();
+                text.setText("0");
+                break;
+            case R.id.result:
+                withResult();
+                break;
+            default:
                 break;
         }
     }
 
+    // 计算结果
+    private void withResult(){
+        if (!ope.equals("")) {  // 进行了运算
+            inputNum_2 = text.getText().toString();
+            double re = 0;  // 结果
+            if (inputNum_2.equals("")) {    // 缺失第二个输入，则第二个输入和第一个输入一样
+                inputNum_2 = inputNum_1;
+            }
+            double input1 = Double.parseDouble(inputNum_1);
+            double input2 = Double.parseDouble(inputNum_2);
+            if (ope.equals("+")){   // 加法
+                re = input1 + input2;
+            }else if (ope.equals("-")){ // 减法
+                re = input1 - input2;
+            }else if (ope.equals("x")) { // 乘法
+                re = input1 * input2;
+            }else {
+                if (input2 != 0.0){
+                    re = input1 / input2;
+                }
+            }
+            inputNum_1 = String.valueOf(re);    // 将结果赋值给   第一个数
+            ope = "";
+            inputNum_2 = "";
+            text.setText(inputNum_1);   // 显示结果
+        }
+
+    }
+
+    // 显示数字在界面上
+    private void showNum(Button button){
+        String s = text.getText().toString();
+        String button_text = button.getText().toString();
+        if (!(s.contains(".") && button_text.equals("."))) {     // 如果不包含小数点或者输入的不是小数点
+            if (s.charAt(0) == '0') { // 去除0
+                if (button_text.equals(".")) {
+                    text.setText(s + button_text);
+                }else{
+                    if (!button_text.equals("0")) {
+                        text.setText(button_text);
+                    }
+                }
+            } else {
+                text.setText(s + button_text);
+            }
+        }
+        Log.d(TAG, "showNum: "+text.getText().toString());
+
+    }
+
+    // 取%（除以100）
+    private String div_100(){
+        String s = text.getText().toString();
+
+        if (s.contains(".")){   // 字符串包含小数点
+            return String.valueOf(Double.parseDouble(s)/100);
+        }else {
+            int num = Integer.parseInt(s);
+            if (num % 100 == 0){    // 能整除100
+                return String.valueOf(num % 100);
+            }else {
+                return String.valueOf(num / 100.0);
+            }
+        }
+    }
+
     // 取反
-    private String negate(String s){
-        String newStr;
+    private String negate(){
+        String s = text.getText().toString();
+        String newStr = "";
+
         if (s.charAt(0) == '-'){
             newStr = s.substring(1);
         }else {
@@ -92,6 +199,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         num_7 = findViewById(R.id.num_7);
         num_8 = findViewById(R.id.num_8);
         num_9 = findViewById(R.id.num_9);
+
+    }
+
+    // 设置监听器
+    private void setClickEvent(){
+
+        clear.setOnClickListener(this);
+        neg.setOnClickListener(this);
+        per.setOnClickListener(this);
+
+        add.setOnClickListener(this);
+        sub.setOnClickListener(this);
+        mul.setOnClickListener(this);
+        div.setOnClickListener(this);
+        result.setOnClickListener(this);
+
+        point.setOnClickListener(this);
+        num_0.setOnClickListener(this);
+        num_1.setOnClickListener(this);
+        num_2.setOnClickListener(this);
+        num_3.setOnClickListener(this);
+        num_4.setOnClickListener(this);
+        num_5.setOnClickListener(this);
+        num_6.setOnClickListener(this);
+        num_7.setOnClickListener(this);
+        num_8.setOnClickListener(this);
+        num_9.setOnClickListener(this);
 
     }
 
